8385c299090521e99b9978490126e9bf6127801e
a1802961 <a1802961@student.adelaide.edu.au>
2024-01-01 15:38:08 +1030

    just the footer left

--- javascript/main.js
+++ javascript/main.js
@@ -12,3 +12,42 @@ $(window).scroll(function () {
         $("header").removeClass("sticky");
     }
 });
+
+
+// Nav-bar
+
+let savedScrollPosition = 0;
+
+function showSidebar() {
+  const sidebar = document.querySelector('.side-bar')
+  sidebar.style.display = 'flex'
+  
+  
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector('.side-bar')
+  savedScrollPosition = window.scrollY || window.pageYOffset;
+  window.scrollTo(0, savedScrollPosition);
+  sidebar.style.display = 'none'
+  
+}
+
+
+
+
+// tips
+
+document.addEventListener("DOMContentLoaded", function() {
+    const sections = document.querySelectorAll(".exp-section");
+  
+    sections.forEach(section => {
+      const header = section.querySelector("h3");
+      const content = section.querySelector(".exp-content");
+  
+      header.addEventListener("click", function() {
+        section.classList.toggle("active");
+        content.style.display = section.classList.contains("active") ? "block" : "none";
+      });
+    });
+  });
